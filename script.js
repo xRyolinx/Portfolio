84d23f083fe93cda362428dfa36c1f543d3844dd
Ryolin <107707022+xRyolinx@users.noreply.github.com>
2023-04-21 19:52:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let i = 0;
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button').addEventListener('click', function() {
-        let decal = document.querySelector('#decal');
+        let main = document.querySelector('main');
+        let home = document.querySelector('#home');
+
         if (i % 2 == 0)
         {
-            decal.style.height = '20vh';
+            main.style.marginTop = '25vh';
+            home.style.marginTop = '2vh';
+            
             i++;
         }
         else
         {
-            decal.style.height = '0';
+            main.style.marginTop = '0';
+            home.style.marginTop = '0';
+            
             i++;
         }
     });
