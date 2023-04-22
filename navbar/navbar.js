67acfa7a890b78ec8a67aaadfa56fd788c8f371b
Ryolin <107707022+xRyolinx@users.noreply.github.com>
2023-04-22 23:13:17 +0100

    Add files via upload

---
+++ navbar/navbar.js
@@ -0,0 +1,52 @@
+//Initialize navbar en fonction de window
+function show_hide_nav()
+{
+    //Hide - Show
+    let nav_add = document.querySelector('#nav_add');
+    let nav_button = document.querySelector('#nav_button');
+
+    // Hide add on mobile
+    if (window.matchMedia("(max-width: 768px)").matches)
+    {
+        nav_add.style.display = 'none';
+        nav_button.style.display = 'block';
+    }
+    // Hide button on pc
+    else
+    {
+        nav_button.before(nav_add);
+        nav_add.style.display = 'flex';
+        nav_button.style.display = 'none';
+    }
+}
+
+let nav_bar_counter = 0;
+document.addEventListener('DOMContentLoaded', function() {
+    //nav_Button in mobile
+    let nav_button = document.querySelector('#nav_button');
+    nav_button.addEventListener('click', function() {
+        let nav = document.querySelector('nav');
+        let nav_add = document.querySelector('#nav_add');
+        //Show
+        if (nav_bar_counter % 2 == 0)
+        {
+            //Suite de navbar
+            nav.after(nav_add);
+            nav_add.style.display = 'flex';
+            nav_bar_counter++;
+        }
+        // Hide
+        else
+        {
+            nav_add.style.display = 'none';
+            nav_bar_counter++;
+        }
+    });
+
+    //Initialize navbar
+    show_hide_nav();
+
+    //Resize
+    window.addEventListener('resize', show_hide_nav);
+
+});
--- script.js
+++ script.js
@@ -1,25 +1,5 @@
 let i = 0;
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button').addEventListener('click', function() {
-        let main = document.querySelector('main');
-        let home = document.querySelector('#home');
-
-        if (i % 2 == 0)
-        {
-            main.style.marginTop = '30vh';
-            home.style.paddingTop = '2vh';
-            
-            i++;
-        }
-        else
-        {
-            main.style.marginTop = '0';
-            home.style.paddingTop = '0';
-            
-            i++;
-        }
-    });
-
     // Inverser bouttons et image
     if ((window.matchMedia("(max-width: 992px)").matches) && (window.innerWidth != '992'))
     {
