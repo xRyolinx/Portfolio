b43b32d231e19c45565a6acff76932036c0fa4be
Ryolin <107707022+xRyolinx@users.noreply.github.com>
2023-04-22 20:10:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -19,9 +19,26 @@ document.addEventListener('DOMContentLoaded', function() {
             i++;
         }
     });
-    if (window.matchMedia("(max-width: 992px)").matches) {
+
+    // Inverser bouttons et image
+    if ((window.matchMedia("(max-width: 992px)").matches) && (window.innerWidth != '992'))
+    {
         let bouttons = document.querySelector('#bouttons');
         let image = document.querySelector('#photo');
         image.after(bouttons);
     }
+    window.addEventListener('resize' ,function() {
+        if ((window.matchMedia("(max-width: 992px)").matches) && (window.innerWidth != '992'))
+        {
+            let bouttons = document.querySelector('#bouttons');
+            let image = document.querySelector('#photo');
+            image.after(bouttons);
+        }
+        else
+        {
+            let bouttons = document.querySelector('#bouttons');
+            let enjoy = document.querySelector('#enjoy');
+            enjoy.after(bouttons);
+        }
+    });
 });
