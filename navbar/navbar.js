a3ba59659ea5eb1d2cb2264737a6d2115392da69
Ryolin <107707022+xRyolinx@users.noreply.github.com>
2023-04-23 01:30:27 +0100

    Add files via upload

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,3 +1,5 @@
+//------------------- functions -----------------------//
+
 //Initialize navbar en fonction de window
 function show_hide_nav()
 {
@@ -20,9 +22,63 @@ function show_hide_nav()
     }
 }
 
+//Changer couleur de bordure top/bot
+function nav_element_color(element, color) {
+    element.style.borderTopColor = color;
+    element.style.borderBottomColor = color;    
+    element.style.color = '#264653';
+}
+
+// Couleur du link de la page actuelle, dans nav
+function nav_element_page(color) {
+    let window_path_name = window.location.pathname;
+    // Home page
+    if (window_path_name == '/')
+    {
+        nav_home = document.querySelector('#nav_home');
+        nav_element_color(nav_home, color);
+        nav_home.style.fontWeight = 'bold';
+    }
+    // Autre pages
+    pages = document.querySelectorAll('.nav_el');
+    pages.forEach(page => {
+        if ((window_path_name.includes(page.firstChild.innerHTML))
+        && page.id != 'nav_home')
+        {
+            nav_element_color(page, color);
+            page.style.fontWeight = 'bold';
+        }    
+    });
+}
+
+//Change la couleur du link de la page actuelle
+function adapt_element_page_size()
+{
+    if (window.matchMedia("(max-width: 992px)").matches)
+    {
+        nav_element_page('transparent');
+    }
+    else
+    {
+        nav_element_page('#264653');
+    }
+}
+
+// Reset la page
+function nav_resize() {
+    show_hide_nav();
+    adapt_element_page_size();
+}
+// --------------------------------------------------- //
+
+
+//Start
 let nav_bar_counter = 0;
 document.addEventListener('DOMContentLoaded', function() {
-    //nav_Button in mobile
+    //Initialize navbar
+    nav_resize();
+
+    //Nav button in mobile
     let nav_button = document.querySelector('#nav_button');
     nav_button.addEventListener('click', function() {
         let nav = document.querySelector('nav');
@@ -43,10 +99,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    //Initialize navbar
-    show_hide_nav();
-
-    //Resize
-    window.addEventListener('resize', show_hide_nav);
-
+    //Reset la page
+    window.addEventListener('resize', nav_resize);
 });
