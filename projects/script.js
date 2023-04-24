4278bed52ab212c0a2a9773e438d569e07e2e1fe
Ryolin <107707022+xRyolinx@users.noreply.github.com>
2023-04-24 05:03:43 +0100

    Add files via upload

--- projects/script.js
+++ projects/script.js
@@ -1,49 +1,4 @@
-//-----------------------------//
-function reset(nb) {
-    let element = document.querySelector('.element');
-    let main_style = document.querySelector('main').style;
-
-    if ((window.matchMedia("(max-width: 600px)").matches))
-    {
-        main_style.gridTemplateColumns = '60vw';
-        element.style.width = '60vw';
-        element.style.height = '100vw';
-        main_style.rowGap = '10vw';
-    }
-    else if ((window.matchMedia("(max-width: 830px)").matches))
-    {
-        main_style.gridTemplateColumns = '30vw 30vw';
-        element.style.width = '30vw';
-        element.style.height = '50vw';
-    }
-    else if ((window.matchMedia("(max-width: 1024px)").matches))
-    {
-        main_style.gridTemplateColumns = '25vw 25vw 25vw';
-        element.style.width = '25vw';
-        element.style.height = '40vw';
-
-    }
-    else
-    {
-        main_style.gridTemplateColumns = '20vw 20vw 20vw 20vw';
-        element.style.width = '20vw';
-        element.style.height = '32vw';
-
-    }
-    
-    for(let i = 0; i < nb - 1 ; i++)
-    {
-        let del = document.getElementById('n' + i);
-        if (del !== null)
-        {
-            del.remove();
-        }
-
-        let new_element = element.cloneNode(true);
-        new_element.id = 'n' + i;
-        element.after(new_element);
-    }
-}
+//----------functions-----------//
 
 // Ajouter elements
 function add_el(nb) {
@@ -58,6 +13,7 @@ function add_el(nb) {
 }
 //-----------------------------//
 
+
 //Global starting elements
 let nb_elements = 8;
 
@@ -70,10 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     //Clone object
-    reset(nb_elements);
-
-    //Resize
-    window.addEventListener('resize', function(){reset(nb_elements);});
+    add_el(nb_elements);
 
     //Voir plus
     document.querySelector('#plus').addEventListener('click', function(event){
