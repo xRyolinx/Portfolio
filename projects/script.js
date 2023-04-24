9c99a4e0ae9117fb276637dd4762bd5f705f2713
Ryolin <107707022+xRyolinx@users.noreply.github.com>
2023-04-24 12:46:45 +0100

    Add files via upload

--- projects/script.js
+++ projects/script.js
@@ -1,13 +1,13 @@
 //----------functions-----------//
 
 // Ajouter elements
-function add_el(nb) {
+function add_el(nb_total, add) {
     let main = document.querySelector('main');
     let element = document.querySelector('.element');
-    for(let i = 1; i < 5 ; i++)
+    for(let i = 0; i < add ; i++)
     {
         let new_element = element.cloneNode(true);
-        new_element.id = 'n' + (i + nb);
+        new_element.id = 'n' + (i + nb_total + 1);
         main.append(new_element);
     }
 }
@@ -15,23 +15,25 @@ function add_el(nb) {
 
 
 //Global starting elements
-let nb_elements = 8;
+let nb_elements = 7;
+let add_element = 4;
 
 //Start
 document.addEventListener('DOMContentLoaded', function() {
     //Starting element of global
     if ((window.matchMedia("(max-width: 1024px)").matches))
     {
-        nb_elements = 4;
+        nb_elements = 2;
+        add_element = 3;
     }
 
     //Clone object
-    add_el(nb_elements);
+    add_el(0, nb_elements);
 
     //Voir plus
     document.querySelector('#plus').addEventListener('click', function(event){
-        add_el(nb_elements);
-        nb_elements += 4;
+        add_el(nb_elements, add_element);
+        nb_elements += add_element;
         event.preventDefault();
     });
 });
